592551e1c89f6595c81cf024e17f95e429cb8712
Ganesh Kumar <51932680+gkumar24@users.noreply.github.com>
2020-01-09 14:13:55 -0600

    UFO 1 completed with filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,39 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// function to populate the table with data.
+function populateTable(tableData) {
+    //find the table body element, using d3 select
+    var tableBody = d3.select("tbody");
+
+    //clear the table body before populating. 
+    tableBody.html("");
+
+    // populate the table, using the data provided
+    tableData.forEach((UFOSighting) => {
+        var tableRow = tableBody.append("tr");
+        Object.entries(UFOSighting).forEach(([key, value]) => {
+            var rowCell = tableRow.append("td");
+            rowCell.text(value);
+        });
+    });
+};
+
+// populate initial data
+populateTable(tableData);
+
+// Filter button event
+var filterButton = d3.select("#filter-btn");
+
+// Filter button event
+filterButton.on("click", function() {
+    // select the data input field, and the value
+    var filterDate = d3.select("#datetime").property("value");
+
+    //given UFO data, filtered by the passed filter date value
+    var filteredData = tableData.filter(ufo => ufo.datetime === filterDate);
+
+    //Call function to populate the table.
+    populateTable(filteredData);
+    
+});
